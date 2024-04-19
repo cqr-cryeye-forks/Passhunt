fe581df2f3f3e1cd36b04333f6d12bd31fed63b0
fabelx <38182176+fabelx@users.noreply.github.com>
2024-04-18 21:25:21 -0400

    Update project

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Passhunt
 Passhunt is a simple tool for searching of default credentials for network devices, web applications and more. Search through 523 vendors and their 2084 default passwords.
 
-# Screenshot
-![image](https://user-images.githubusercontent.com/3501170/36933533-92b1c136-1f2e-11e8-80b7-b76fbbbfe54b.png)
+# Usage 
+```bash
+usage: passhunt.py [-h] -t TARGET [-o OUTPUT]
+
+options:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        Target for passhunt
+  -o OUTPUT, --output OUTPUT
+                        Output file path (json format)
+```
 
 # Pre-requisites
 Make sure you have installed the following:
@@ -19,15 +28,4 @@ pip3 install -r requirements.txt
 python3 Passhunt.py
 </pre>
 
-# How do I use this?
-- Press 1: This will print the list of supported vendors.
-- Press 2: Enter the vendor name and search for default credentials.
-- Press 3: To exit from the program.
-
-# Credit
-The list of default passwords is obtained from cirt.net. All passwords and vendor list maintained by cirt.net
-
-# TODO
-- Offline password search
-- Create username and password list in a json file and parse them
 
--- passhunt.py
+++ passhunt.py
@@ -1,110 +1,205 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python3
-'''
-This tool allows you to search for default credentials for routers, network devices, web applications and more. 
-Author: Viral Maniar 
-Twitter: https://twitter.com/maniarviral
-Github: https://github.com/Viralmaniar
-LinkedIn: https://au.linkedin.com/in/viralmaniar
-'''
-import os, sys
-import urllib.request
-import io
+# !/usr/bin/env python3
+"""
+This tool allows you to search for default credentials for routers, network devices,
+    web applications and more.
+
+# python passhunt.py -t joomla.afganii.fun
+# python passhunt.py -t testphp.vulnweb.com
+
+Source idea of passhunt:
+    Author: Viral Maniar
+    Twitter: https://twitter.com/maniarviral
+    Github: https://github.com/Viralmaniar
+    LinkedIn: https://au.linkedin.com/in/viralmaniar
+"""
+import json
+from argparse import ArgumentParser
+
 import bs4 as bs
-import ssl
-	
-def logo():
-	logo = ''''''
-	return logo
+import nmap3
+import requests
 
-OPTIONS = '''
-1. List supported vendors
-2. Search Default Password
-3. Exit
-'''
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
-	
-def menu():
-	while True:
-		try:
-			choice = str(input('\n[?] Do you want to continue? \n> ')).lower()
-			if choice[0] == 'y':
-				return
-			if choice[0] == 'n':
-				sys.exit(0)
-				break
-		except ValueError:
-			sys.exit(0)
+requests.packages.urllib3.disable_warnings()
+SERVICE_URL = "https://cirt.net"
+VENDORS_URL = f"{SERVICE_URL}/passwords?vendor="
 
-def checkInternetConnection():
-		try:
-			urllib.request.urlopen('https://cirt.net/')
-		except:
-			print('[!] No internet connection...Please connect to the Internet')
-		else:
-			print('[+] Checking Internet connection...')
-						
-def formatTable(table):
-    text = ''
-    rows = table.find_all('tr')
-    text += '%s\n' % rows[0].text
-
-    for row in rows[1:]:
-        data = row.find_all('td')
-        text += '%s: %s\n' % (data[0].text, data[1].text)
-
-    return text
-
-def cmd_vendorSearch():
-
-	vendor = input('Enter Vendor Name:').lower()
-	urlenc = urllib.parse.quote(vendor)
-	url = "https://cirt.net/passwords?vendor=" + urlenc
-	request = urllib.request.Request(url)
-	response = urllib.request.urlopen(request)
-	#response = urllib.quote(request)
-	#print (response.read().decode('utf-8'))
-	soup = bs.BeautifulSoup(response, "html.parser")
-	#print(soup.find_all('a'))
-	for links in soup.find_all('table'):
-		#print(links.text)
-		print(formatTable(links))
-
-def cmd_openFile():	
-	path = './vendors.txt'
-	vendors_file = open(path,'r')
-	vendors = vendors_file.read()
-	print(vendors)
-	
-cmds = {
-	"1" : cmd_openFile,
-	"2"	: cmd_vendorSearch,
-	"3"	: lambda: sys.exit(0)
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
 }
 
+
+def check_service():
+    try:
+        resp = requests.get(SERVICE_URL, headers=HEADERS, verify=False)
+        if resp.status_code != 200:
+            raise Exception
+    except Exception:
+        print('cirt.net service is not available, exiting...')
+        exit(1)
+    else:
+        print('cirt.net service is available, continue...')
+
+
+def parse_vendors(data: str) -> list:
+    soup = bs.BeautifulSoup(data, 'html.parser')
+    vendors = soup.find_all('td')
+    return [v.text.lower() for v in vendors if v]
+
+
+def get_vendors() -> list:
+    resp = requests.get(VENDORS_URL, headers=HEADERS, verify=False)
+    if resp.status_code != 200:
+        print("failed to retrieve vendors from cirt.net, exiting...")
+        exit(1)
+
+    return parse_vendors(resp.content.decode('utf-8'))
+
+
+def cli(parser: ArgumentParser):
+    parser.add_argument(
+        '-t', '--target',
+        required=True,
+        help="Target for passhunt"
+    )
+    parser.add_argument(
+        '-o', '--output',
+        default='output.json',
+        help="Output file path (json format)"
+    )
+    return parser.parse_args()
+
+
+def parse_nmap_result(services: dict) -> list:
+    for service in services.values():
+        if service.get('ports'):
+            services = service.get('ports')
+            break
+    # probably, can produce ['', '', '', ...], but that's okay
+    return [s.get('service', {}).get('product', '').lower() for s in services if s]
+
+
+def run_nmap(host: str) -> list:
+    try:
+        nmap = nmap3.Nmap()
+        result = nmap.nmap_version_detection(host)
+        # result = [
+        #     {
+        #         "cpe": [
+        #             {
+        #                 "cpe": "cpe:/o:linux:linux_kernel"
+        #             }
+        #         ],
+        #         "port": "80",
+        #         "protocol": "tcp",
+        #         "service": {
+        #             "conf": "10",
+        #             "extrainfo": "Ubuntu",
+        #             "method": "probed",
+        #             "name": "http",
+        #             "ostype": "Linux",
+        #             "product": "nginx",
+        #             "version": "1.14.0"
+        #         }
+        #     },
+        #     {
+        #         "cpe": [
+        #             {
+        #                 "cpe": "cpe:/o:linux:linux_kernel"
+        #             }
+        #         ],
+        #         "port": "443",
+        #         "protocol": "tcp",
+        #         "service": {
+        #             "conf": "10",
+        #             "extrainfo": "Ubuntu",
+        #             "method": "probed",
+        #             "name": "http",
+        #             "ostype": "Linux",
+        #             "product": "nginx",
+        #             "tunnel": "ssl",
+        #             "version": "1.14.0"
+        #         }
+        #     },
+        #     {
+        #         "cpe": [
+        #             {
+        #                 "cpe": "cpe:/o:linux:linux_kernel"
+        #             }
+        #         ],
+        #         "port": "2000",
+        #         "protocol": "tcp",
+        #         "service": {
+        #             "conf": "10",
+        #             "extrainfo": "Ubuntu Linux; protocol 2.0",
+        #             "method": "probed",
+        #             "name": "ssh",
+        #             "ostype": "Linux",
+        #             "product": "OpenSSH",
+        #             "version": "7.6p1 Ubuntu 4ubuntu0.3"
+        #         }
+        #     }
+        # ]
+        return parse_nmap_result(result)
+
+    except Exception as e:
+        print(e)
+        exit(1)
+
+
+def parse_vendor_info(data: str) -> list:
+    output = []
+    soup = bs.BeautifulSoup(data, 'html.parser')
+    table = soup.find_all('table')
+    for item in table:
+        info = {}
+        tr = item.find_all('tr')[1:]
+        try:
+            for tr_ in tr:
+                tds = tr_.find_all('td')
+                info.update({
+                    tds[0].text.lower().strip(): tds[1].text.strip()
+                })
+            output.append(info)
+        except Exception:
+            continue
+
+    return output
+
+
+def get_info(vendor: str) -> list:
+    resp = requests.get(f"{VENDORS_URL}{vendor}", headers=HEADERS, verify=False)
+    if resp.status_code != 200:
+        print("failed to retrieve vendors from cirt.net, exiting...")
+        exit(1)
+
+    return parse_vendor_info(resp.content.decode('utf-8'))
+
+
 def main():
-	print (logo())
-	checkInternetConnection()
-	try:
-		while True:
-			choice = input("\n%s" % OPTIONS)
-			if choice not in cmds:
-				print ('[!] Invalid Choice')
-				continue
-			cmds.get(choice)()
-	except KeyboardInterrupt:
-		print ('[!] Ctrl + C detected\n[!] Exiting')
-		sys.exit(0)
-	except EOFError:
-		print ('[!] Ctrl + D detected\n[!] Exiting')
-		sys.exit(0)
+    parser = ArgumentParser()
+    args = cli(parser)
+    check_service()
+    vendors = get_vendors()
+    services = run_nmap(args.target)
+    print(f"Found services: {services}")
+    output = []
+    for s in services:
+        if s in vendors:
+            output.extend(get_info(s))
+
+    print(f"Output: {output}")
+    with open(args.output, 'w') as f:
+        json.dump(output, f, indent=2)
+
 
 if __name__ == "__main__":
-	main()
+    main()
